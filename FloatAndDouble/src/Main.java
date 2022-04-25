30410ab01098807ab521e68f941f74081e18c4c3
Don-Cryptus <don.cryptus@gmail.com>
2022-04-25 23:55:21 +0200

    FloatAndDouble challenge finished

--- FloatAndDouble/src/Main.java
+++ FloatAndDouble/src/Main.java
@@ -18,5 +18,13 @@ public class Main {
         System.out.println("MyFloatValue = " + myFloatValue);
         System.out.println("MyDoubleValue = " + myDoubleValue);
 
+        double numberOfPounds = 200d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }
