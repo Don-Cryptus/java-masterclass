1c84f0463184d87bc2bd61b5d258e38c15380dee
Don-Cryptus <don.cryptus@gmail.com>
2022-04-25 21:17:57 +0200

    finished hello & add new project

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,9 +1,15 @@
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Hello, Don");
 
-        System.out.printf("Hello world");
+        final int myFirstNumber = (10 + 5) + (2 * 10);
+        final int mySecondNumber = 12;
+        final int myThirdNumber = myFirstNumber * 2;
+        final int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        final int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
         
-        final int myFirstVar = 5;
     }
 }
